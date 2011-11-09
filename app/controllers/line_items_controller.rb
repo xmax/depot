daa14d013320ca9d17776830f6e8d0f27461ea74
Max N. Bielikov <ua.xmax@gmail.com>
2011-11-09 23:25:40 +0200

    Chapter F - Time2sleep

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -42,7 +42,7 @@ class LineItemsController < ApplicationController
 		@line_item = @cart.add_product(product.id)
 		respond_to do |format|
 			if @line_item.save
-				format.html { redirect_to @line_item.cart }
+				format.html { redirect_to store_url }
 				format.json { render json: @line_item,
 							status: :created, location: @line_item }
 			else
