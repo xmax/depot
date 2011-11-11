73ce349aafc87e3927243ff18a94094078c20feb
Max N. Bielikov <ua.xmax@gmail.com>
2011-11-11 16:57:00 +0200

    Complete G

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,83 +1,91 @@
 class ProductsController < ApplicationController
-  # GET /products
-  # GET /products.json
-  def index
-    @products = Product.all
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @products }
-    end
-  end
+	# GET /products
+	# GET /products.json
+	def index
+		@products = Product.all
 
-  # GET /products/1
-  # GET /products/1.json
-  def show
-    @product = Product.find(params[:id])
+		respond_to do |format|
+			format.html # index.html.erb
+			format.json { render json: @products }
+		end
+	end
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @product }
-    end
-  end
+	# GET /products/1
+	# GET /products/1.json
+	def show
+		@product = Product.find(params[:id])
 
-  # GET /products/new
-  # GET /products/new.json
-  def new
-    @product = Product.new
+		respond_to do |format|
+			format.html # show.html.erb
+			format.json { render json: @product }
+		end
+	end
 
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @product }
-    end
-  end
+	# GET /products/new
+	# GET /products/new.json
+	def new
+		@product = Product.new
 
-  # GET /products/1/edit
-  def edit
-    @product = Product.find(params[:id])
-  end
+		respond_to do |format|
+			format.html # new.html.erb
+			format.json { render json: @product }
+		end
+	end
 
-  # POST /products
-  # POST /products.json
-  def create
-    @product = Product.new(params[:product])
+	# GET /products/1/edit
+	def edit
+		@product = Product.find(params[:id])
+	end
 
-    respond_to do |format|
-      if @product.save
-        format.html { redirect_to @product, notice: 'Product was successfully created.' }
-        format.json { render json: @product, status: :created, location: @product }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+	# POST /products
+	# POST /products.json
+	def create
+		@product = Product.new(params[:product])
 
-  # PUT /products/1
-  # PUT /products/1.json
-  def update
-    @product = Product.find(params[:id])
+		respond_to do |format|
+			if @product.save
+				format.html { redirect_to @product, notice: 'Product was successfully created.' }
+				format.json { render json: @product, status: :created, location: @product }
+			else
+				format.html { render action: "new" }
+				format.json { render json: @product.errors, status: :unprocessable_entity }
+			end
+		end
+	end
 
-    respond_to do |format|
-      if @product.update_attributes(params[:product])
-        format.html { redirect_to @product, notice: 'Product was successfully updated.' }
-        format.json { head :ok }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+	# PUT /products/1
+	# PUT /products/1.json
+	def update
+		@product = Product.find(params[:id])
 
-  # DELETE /products/1
-  # DELETE /products/1.json
-  def destroy
-    @product = Product.find(params[:id])
-    @product.destroy
+		respond_to do |format|
+			if @product.update_attributes(params[:product])
+				format.html { redirect_to @product, notice: 'Product was successfully updated.' }
+				format.json { head :ok }
+			else
+				format.html { render action: "edit" }
+				format.json { render json: @product.errors, status: :unprocessable_entity }
+			end
+		end
+	end
 
-    respond_to do |format|
-      format.html { redirect_to products_url }
-      format.json { head :ok }
-    end
-  end
+	# DELETE /products/1
+	# DELETE /products/1.json
+	def destroy
+		@product = Product.find(params[:id])
+		@product.destroy
+
+		respond_to do |format|
+			format.html { redirect_to products_url }
+			format.json { head :ok }
+		end
+	end
+
+	def who_bought
+		@product = Product.find(params[:id])
+		respond_to do |format|
+			format.atom
+		end
+	end
 end
